6cf50536a001bc3f773ae83bf0e52295e2ff3d84
iolinpic <iolinpic@gmail.com>
2019-08-08 18:03:00 +0300

    small validation update

--- src/models/user.js
+++ src/models/user.js
@@ -14,10 +14,11 @@ const userSchema = mongoose.Schema({
         required: true,
         unique: true,
         lowercase: true,
-        validate: value => {
-            if(!validator.isEmail(value)){
-                throw new Error({error: 'Invalid email adress'})
-            }
+        validate: {
+            validator: value => {
+                return validator.isEmail(value);
+            },
+            message:props => `${props.value} Is not valid email adress!`
         }
     },
     password: {
@@ -25,8 +26,8 @@ const userSchema = mongoose.Schema({
         required: true,
         minLength: 7
     },
-    tokens:[{
-        token:{
+    tokens: [{
+        token: {
             type: String,
             required: true,
         }
@@ -42,29 +43,29 @@ userSchema.methods.toJSON = function () {
 userSchema.pre('save', async function (next) {
     //hash password before saving
     const user = this;
-    if(user.isModified('password')){
-        user.password = await bcrypt.hash(user.password,12);
+    if (user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 12);
     }
     next();
 });
 userSchema.methods.generateAuthToken = async function () {
     const user = this;
-    const token = jwt.sign({_id:user._id},process.env.JWT_KEY);
+    const token = jwt.sign({_id: user._id}, process.env.JWT_KEY);
     user.tokens = user.tokens.concat({token});
     await user.save();
     return token;
 };
 userSchema.statics.findByCredentials = async (email, password) => {
     // Search for a user by email and password.
-    const user = await User.findOne({ email} );
+    const user = await User.findOne({email});
     if (!user) {
-        throw new Error({ error: 'Invalid login credentials' })
+        throw new Error({error: 'Invalid login credentials'})
     }
     const isPasswordMatch = await bcrypt.compare(password, user.password);
     if (!isPasswordMatch) {
-        throw new Error({ error: 'Invalid login credentials' })
+        throw new Error({error: 'Invalid login credentials'})
     }
     return user
 };
-const User = mongoose.model('User',userSchema);
+const User = mongoose.model('User', userSchema);
 module.exports = User;
