9a8bb2d83420ad6712f3d2d6cafe9a215facd884
iolinpic <iolinpic@gmail.com>
2019-10-09 10:55:19 +0300

    sml fix in code style

--- src/config/db.js
+++
@@ -1,7 +0,0 @@
-
-// .then(() => {
-//   console.log('Successfully connected to the database');
-// }).catch((err) => {
-//   console.log('Could not connect to the database. Exiting now...', err);
-//   process.exit();
-// });
--- src/config/logger.js
+++ src/config/logger.js
@@ -21,8 +21,6 @@ const logger = createLogger({
     }),
     format.json(),
   ),
-  // You can also comment out the line above and uncomment the line below for JSON format
-  // format: format.json(),
   transports: [
     new transports.Console({
       level: 'info',
