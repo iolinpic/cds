4981a1e0d44955d539c2b995ad7026a7ec8ad0a8
iolinpic <iolinpic@gmail.com>
2020-09-02 19:15:36 +0300

    file upload upgrade

--- src/controllers/file.js
+++ src/controllers/file.js
@@ -4,13 +4,14 @@ const path = require('path')
 exports.upload = async (req, res) => {
     try {
         let fstream;
-        let dir = 'public/static/';
+        let defaultDir = 'public/'
+        let dir = 'static/';
         if (!fs.existsSync(dir)) {
             fs.mkdirSync(dir);
         }
         req.pipe(req.busboy);
         req.busboy.on('file', function (fieldname, file, filename) {
-            fstream = fs.createWriteStream(dir+filename);
+            fstream = fs.createWriteStream(defaultDir+dir+filename);
             file.pipe(fstream);
             fstream.on('close', function () {
                 res.status(200).send(JSON.stringify({path:dir+filename}));           //where to go next
